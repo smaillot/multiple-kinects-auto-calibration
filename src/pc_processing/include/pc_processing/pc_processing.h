f6019d121a54d07abfd77ab9a1f4621237f1f279
Stéphane Maillot <s.maillot2@gmail.com>
2018-02-15 19:02:50 +0100

    add radius filtering

--- src/pc_processing/include/pc_processing/pc_processing.h
+++ src/pc_processing/include/pc_processing/pc_processing.h
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <ctime>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
@@ -9,32 +10,45 @@
 #include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <pcl/common/transforms.h>
+#include <pcl/filters/radius_outlier_removal.h>
 // synchronization
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+// messages
+#include <sensor_msgs/PointCloud2.h>
 // tf
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 // dynamic reconfigure
 #include <dynamic_reconfigure/server.h>
-#include <multiple_kinects/subsamplingConfig.h>
+#include <pc_processing/registrationConfig.h>
 
 
-class pc_processing
+class PcProcessing
 {
 private:
+	// parameters
 	double subsize;
+	
+	bool filtering;
+	double filter_radius;
+	int filter_min_neighbors;
+
+	// tf listener
 	const tf::TransformListener* tf_listener;
+
+	// point clouds
 	sensor_msgs::PointCloud2* full_pc;
 	sensor_msgs::PointCloud2* filtered_pc;
 
 public:
-	pc_processing();
-	virtual ~pc_processing();
+	PcProcessing();
+	virtual ~PcProcessing();
 	void merge_pc(const sensor_msgs::PointCloud2ConstPtr& pc1, const sensor_msgs::PointCloud2ConstPtr& pc2);
 	void subsample_pc();
 	void set_subsize(double subsize);
+	void set_filtering_params(bool filtering, double filter_radius, int filter_min_neighbors);
 	double get_subsize();
 	void set_listener(const tf::TransformListener* listener);
 	sensor_msgs::PointCloud2* get_filtered_pc();
--- src/pc_processing/src/pc_processing_nodelet.cpp
+++ src/pc_processing/src/pc_processing_nodelet.cpp
@@ -6,37 +6,62 @@ using namespace std;
 
 tf::TransformListener *listener;
 ros::Publisher pub;
-pc_processing PC_object;
+PcProcessing PC_object;
+
+//  parameters
+//      subsampling
 double subsize;
+//      filtering
+bool filtering;
+double filter_radius;
+int filter_min_neighbors;
+
 
 void pc_callback(const PointCloud2ConstPtr& pc1, const PointCloud2ConstPtr& pc2)
 {
+    // merging piont clouds
     PC_object.merge_pc(pc1, pc2);
+
+    // subsampling point cloud
     PC_object.set_subsize(subsize);
     PC_object.subsample_pc();
 
+    // filtering point cloud
+    PC_object.set_filtering_params(filtering, filter_radius, filter_min_neighbors);
+    PC_object.filter_pc();
 
     PointCloud2* ptr = PC_object.get_filtered_pc() ;
     pub.publish(*ptr);
 }
 
-void dynrec_callback(multiple_kinects::subsamplingConfig &config, uint32_t level)
+void dynrec_callback(pc_processing::registrationConfig &config, uint32_t level)
 {
-    subsize = config.subsampling * config.size / 100;
+    // subsampling
+    subsize = config.subsampling_enable * config.subsampling_size / 100;
+
+    // filtering
+    filtering = config.filter_enable;
+    filter_radius = config.filter_radius / 100;
+    filter_min_neighbors = config.filter_min_neighbors;
 }
 
 int main(int argc, char** argv)
 {
+    // verbosity
+    if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) 
+    {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
     // Initialize ROS
     ros::init(argc, argv, "pc_processing_nodelet");
     ros::NodeHandle nh;
-    nh.getParam("pc_subsampling_size", subsize);
     listener = new tf::TransformListener();
     PC_object.set_listener(listener);
 
     // dynamic reconfigure
-    dynamic_reconfigure::Server<multiple_kinects::subsamplingConfig> server;
-    dynamic_reconfigure::Server<multiple_kinects::subsamplingConfig>::CallbackType f;
+    dynamic_reconfigure::Server<pc_processing::registrationConfig> server;
+    dynamic_reconfigure::Server<pc_processing::registrationConfig>::CallbackType f;
     f = boost::bind(&dynrec_callback, _1, _2);
     server.setCallback(f);
 
@@ -47,7 +72,7 @@ int main(int argc, char** argv)
     Synchronizer<KinectSync> sync(KinectSync(10), cam1, cam2);
     sync.registerCallback(boost::bind(&pc_callback, _1, _2));
 
-    pub = nh.advertise<PointCloud2>("/kinects/qhd/points", 1);
+    pub = nh.advertise<PointCloud2>("/scene/qhd/points", 1);
 
     // Spin
     ros::spin();
