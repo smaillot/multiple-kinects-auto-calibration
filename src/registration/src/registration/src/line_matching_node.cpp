4be67ca457d7ea36d3e5989f69e84f138cf499c7
Stéphane Maillot <s.maillot2@gmail.com>
2018-02-28 17:57:49 +0100

    create draft file

--- src/registration/src/registration/src/line_matching_node.cpp
+++ src/registration/src/registration/src/line_matching_node.cpp
@@ -1,4 +1,4 @@
-#include <plane_detection/plane_detection_node.h>
+#include <registration/plane_matching_node.h>
 using namespace std;
 #include <iostream>
 
@@ -6,17 +6,15 @@ using namespace std;
 *	args parser
 *
 *	node frequency
-*	input list
-*	number of planes
+*   input list
 *	input topics namespace
-*	input topics name
 *	output topics namespace
 */
 
 const string inputs[2] = {"/cam1", "/cam2"};
 const int n_inputs = sizeof(inputs) / sizeof(*inputs);
-const string sub_topic_name = "/reconstruction/point_clouds";
-const string pub_topic_name = "/reconstruction/planes";
+const string sub_topic_name = "/reconstruction/planes";
+const string pub_topic_name = "/reconstruction/lines";
 float frequency = 0;
 vector<geometry::PointCloud*> PC;
 
@@ -28,7 +26,7 @@ vector<geometry::PointCloud*> PC;
  */
 std::string get_topic_name(int input_number)
 {
-	return sub_topic_name + inputs[input_number];
+	return sub_topic_name + inputs[input_number] + "/plane" + patch::to_string(i);
 }
 
 /**
@@ -41,23 +39,23 @@ std::string get_publish_name(int input_number)
 	return pub_topic_name + inputs[input_number];
 }
 
-/**
- * @brief Callback for plane detection parameters dynamic reconfigure reconfigure.
- */
-void plane_detection_conf_callback(plane_detection::PlaneDetectionConfig &config, uint32_t level)
-{
-    bool enabled = config.enabled;
-	int n_planes = config.n_planes;
-	float th_dist = config.th_dist / 1000;
-	int max_it = config.max_it;
+// /**
+//  * @brief Callback for plane matching parameters dynamic reconfigure reconfigure.
+//  */
+// void plane_matching_conf_callback(plane_matching::PlaneMatchingConfig &config, uint32_t level)
+// {
+//     bool enabled = config.enabled;
+// 	int n_planes = config.n_planes;
+// 	float th_dist = config.th_dist / 1000;
+// 	int max_it = config.max_it;
 
-	for (int i = 0 ; i < n_inputs ; i++)
-	{
-		PD[i]->set_params(enabled, n_planes, th_dist, max_it);
-	}
+// 	for (int i = 0 ; i < n_inputs ; i++)
+// 	{
+// 		PD[i]->set_params(enabled, n_planes, th_dist, max_it);
+// 	}
 
-    ROS_DEBUG("Plane detection parameters config updated");
-}
+//     ROS_DEBUG("Plane detection parameters config updated");
+// }
 
 int main(int argc, char *argv[])
 {
@@ -68,14 +66,14 @@ int main(int argc, char *argv[])
 		}
 
 	// Initialize ROS
-		ros::init(argc, argv, "plane_detection_node");
+		ros::init(argc, argv, "plane_matching_node");
 		ros::NodeHandle nh;
-		ros::NodeHandle node_ransac("~/RANSAC");
+		// ros::NodeHandle node_ransac("~/RANSAC");
 
-	// create PlaneDetector objects
+	// create PointCloud objects
 		for (int i = 0 ; i < n_inputs ; i++)
 		{
-			PD.push_back(new geometry::PlaneDetector(nh, get_topic_name(i), get_publish_name(i)));
+			PC.push_back(new geometry::PlaneDetector(nh, get_topic_name(i), get_publish_name(i)));
 		}
 
 	// dynamic reconfigure
