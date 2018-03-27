c53b905ed315830073e78a26365e61ae5617a3da
Stéphane Maillot <s.maillot2@gmail.com>
2018-03-27 18:35:12 +0200

    add reconfigure for merging frames

--- src/registration/src/registration/include/registration/merging_node.h
+++ src/registration/src/registration/include/registration/merging_node.h
@@ -22,3 +22,6 @@
 	#include <message_filters/subscriber.h>
 	#include <message_filters/time_synchronizer.h>
 	#include <message_filters/sync_policies/approximate_time.h>
+// dyn rec
+	#include <dynamic_reconfigure/server.h>
+	#include <registration/MergingConfig.h>
--- src/registration/src/registration/src/merging_node.cpp
+++ src/registration/src/registration/src/merging_node.cpp
@@ -14,6 +14,7 @@ using namespace message_filters;
 vector <string> inputs; // {"/reconstruction/point_clouds/cam1", "/reconstruction/point_clouds/cam2"};
 //const int n_inputs = sizeof(inputs) / sizeof(*inputs);
 const string topic_namespace = "/reconstruction/point_clouds";
+string frame[2];
 string output_name; // = "fix";
 float frequency = 0;
 ros::Publisher pub_pc;
@@ -43,19 +44,24 @@ void pc_callback(const PointCloud2ConstPtr& pc1, const PointCloud2ConstPtr& pc2)
     PointCloud2 input1 = *pc1;
     PointCloud2 input2 = *pc2;
     PointCloud2 merged_pc;
-    // std::string target_tf = "/cam_center";
-    // try
-    // {
-    //     ros::Time t = ros::Time(0);
-    //     pcl_ros::transformPointCloud(target_tf, input2, input2, *listener);
-    // }
-	// catch (...)
-	// {
-	// 	ROS_WARN("Error while transforming point cloud");
-	// }
+    try
+    {
+        pcl_ros::transformPointCloud(frame[0], input1, input1, *listener);
+        pcl_ros::transformPointCloud(frame[1], input2, input2, *listener);
+    }
+    catch (...)
+    {
+    	ROS_WARN("Error while transforming point cloud");
+    }
     pcl::concatenatePointCloud(input1, input2, merged_pc);
 
     pub_pc.publish(merged_pc);
+}
+
+void dynrec_callback(registration::MergingConfig &config, uint32_t level)
+{
+    frame[0] = config.frame1;
+    frame[1] = config.frame2;
 }
 
 int main(int argc, char *argv[])
@@ -71,8 +77,8 @@ int main(int argc, char *argv[])
 		// TODO error catching 
 
 		inputs.clear();
-        string node_name = "merging_";
-        node_name += argv[1];
+        string name = argv[1];
+        string node_name = "merging_" + name;
 		int i = 2;
 		while (i < argc-1) 
 		{
@@ -84,7 +90,13 @@ int main(int argc, char *argv[])
     // Initialize ROS
         ros::init(argc, argv, node_name);
         ros::NodeHandle nh;
+        ros::NodeHandle node_config("postprocessing/" + name + "/merging");
     
+    // dynamic reconfigure
+        dynamic_reconfigure::Server<registration::MergingConfig> server(node_config);
+        dynamic_reconfigure::Server<registration::MergingConfig>::CallbackType f;
+        f = boost::bind(&dynrec_callback, _1, _2);
+        server.setCallback(f);
 
     // Synchronize both kinects messages
         message_filters::Subscriber<PointCloud2> cam1(nh, get_topic_name(0), 1);
