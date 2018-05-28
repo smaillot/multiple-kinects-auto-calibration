529a0aed329ad86ac289facec2819a7be3cd0cbb
Stéphane Maillot <s.maillot2@gmail.com>
2018-05-28 16:59:57 +0200

    complete transform estimation

--- src/calib/src/TransformEstimator.cpp
+++ src/calib/src/TransformEstimator.cpp
@@ -16,6 +16,7 @@ void TransformEstimator::addPoints(vector<Eigen::Vector3f> points, bool source)
     {
         for (int i = 0; i < points.size(); i++)
         {
+            ROS_DEBUG_STREAM("Adding point: " << points[i].transpose());
             this->points_source.push_back(points[i]);
         }
     }
@@ -23,6 +24,7 @@ void TransformEstimator::addPoints(vector<Eigen::Vector3f> points, bool source)
     {
         for (int i = 0; i < points.size(); i++)
         {
+            ROS_DEBUG_STREAM("Adding point: " << points[i].transpose());
             this->points_target.push_back(points[i]);
         }
     }
@@ -47,6 +49,7 @@ void TransformEstimator::addPlanes(vector<Eigen::Vector4f> planes, bool source)
     }
     for (int i = 0; i < planes.size(); i++)
     {
+        ROS_DEBUG_STREAM("Adding plane: " << planes[i].transpose() << " with weight " << 1);
         p->push_back(planes[i]);
     }
     while (this->planes_weights.size() < p->size())
@@ -68,6 +71,7 @@ void TransformEstimator::addPlanes(vector<Eigen::Vector4f> planes, bool source, 
     }
     for (int i = 0; i < planes.size(); i++)
     {
+        ROS_DEBUG_STREAM("Adding plane: " << planes[i].transpose() << " with weight " << w);
         p->push_back(planes[i]);
     }
     while (this->planes_weights.size() < p->size())
@@ -81,6 +85,7 @@ void TransformEstimator::addPlanes(vector<Eigen::Vector4f> planes, bool source, 
 */
 void TransformEstimator::reset()
 {
+    ROS_DEBUG("Reset matches");
     this->planes_weights.clear();
     this->points_source.clear();
     this->points_target.clear();
@@ -96,8 +101,10 @@ Eigen::Matrix3f TransformEstimator::getRotation()
     Eigen::Matrix3f K = this->computeRotCorr();
     Eigen::JacobiSVD<Eigen::Matrix3f> svd(K, Eigen::ComputeFullV | Eigen::ComputeFullU);
 	Eigen::Matrix3f R = svd.matrixU() * svd.matrixV().transpose();
+    ROS_DEBUG_STREAM("Rotation:" << endl << R);
     Eigen::DiagonalMatrix<float, 3> D(1, 1, R.determinant());
 	R = svd.matrixU() * D * svd.matrixV().transpose();
+    ROS_DEBUG_STREAM("Regularized rotation:" << endl << R);
 
     return R;
 }
@@ -148,18 +155,26 @@ Eigen::Vector3f TransformEstimator::getTranslation(Eigen::Matrix3f R)
                 Eigen::VectorXf b(N+3);
                 A << A1, A2;
                 b << b1, b2;
+                ROS_DEBUG_STREAM("A =" << endl << A);
+                ROS_DEBUG_STREAM("b =" << endl << b);
 
                 t = (A.transpose() * A).inverse() * A.transpose() * b;
             }
             else // only planes
             {
+                ROS_DEBUG_STREAM("A =" << endl << A2);
+                ROS_DEBUG_STREAM("b =" << endl << b2);
                 t = (A2.transpose() * A2).inverse() * A2.transpose() * b2;
             }
         }
         else // only points
         {
+            ROS_DEBUG_STREAM("A =" << endl << A1);
+            ROS_DEBUG_STREAM("b =" << endl << b1);
             t = (A1.transpose() * A1).inverse() * A1.transpose() * b1;
         }
+
+        ROS_DEBUG_STREAM("t =" << endl << t);
         return t;
     }
 }
@@ -187,6 +202,7 @@ Eigen::Affine3d TransformEstimator::getTransform(bool transl, bool rot)
     ROS_INFO_STREAM("transform =" << endl << H);
     Eigen::Affine3d T;
     T.matrix() = H.cast<double>();
+    ROS_DEBUG_STREAM("H =" << endl << H);    
     return T;
 }
 
@@ -205,13 +221,16 @@ Eigen::Affine3d TransformEstimator::getTransform()
 */
 Eigen::Vector3f TransformEstimator::computeCentroid(vector <Eigen::Vector3f> vec)
 {
-    Eigen::Vector3f centroid;
+    Eigen::Vector3f centroid(0, 0, 0);
     int n = vec.size();
     for (int i = 0; i < n; i++)
     {
+        ROS_DEBUG_STREAM("\tAdding\t" << vec[i].transpose());
+        ROS_DEBUG_STREAM("\t\tCurrent centroid\t" << centroid.transpose());
         centroid += vec[i];
     }
     centroid /= (float) n;
+    ROS_DEBUG_STREAM("Points centroid: " << centroid.transpose());
     return centroid;
 }
 
@@ -244,6 +263,7 @@ Eigen::Matrix3f TransformEstimator::computeRotCorr()
             }
         }
     }
+    ROS_DEBUG_STREAM("rotation correlation =" << endl << K);
     return K;
 }
 
@@ -252,11 +272,23 @@ Eigen::Matrix3f TransformEstimator::computeRotCorr()
 */
 bool TransformEstimator::isValid()
 {
-    if (points_source.size() != points_target.size()) {return false;};
-    if (planes_source.size() != planes_target.size()) {return false;};
-    if (planes_source.size() != planes_weights.size()) {return false;};
-    if (planes_source.size() + points_source.size() < 3) {return false;};
-    return true;
+    bool valid = true;
+    if (points_source.size() != points_target.size()) {valid = false;};
+    if (planes_source.size() != planes_target.size()) {valid = false;};
+    if (planes_source.size() != planes_weights.size()) {valid = false;};
+    if (planes_source.size() + points_source.size() < 3) {valid = false;};
+    if (valid)
+    {
+        ROS_DEBUG("Valid inputs !");
+    }
+    else
+    {
+        ROS_ERROR_STREAM("Invalid input for transform estimation:");
+        ROS_ERROR_STREAM("\tPoints: " << points_source.size() << ", " << points_target.size());
+        ROS_ERROR_STREAM("\tPlanes: " << planes_source.size() << ", " << planes_target.size());
+        ROS_ERROR_STREAM("\tWeights: " << planes_weights.size());
+    }
+    return valid;
 }
 
 // int main(int argc, char *argv[])
--- src/calib/src/transform_estimation_node.cpp
+++ src/calib/src/transform_estimation_node.cpp
@@ -1,85 +1,75 @@
 #include "transform_estimation_node.h"
  
 using namespace std;
-using namespace message_filters;
  
 float freq = 0;
-ros::Publisher pub;
-calib::Planes planes1;
-calib::Planes planes2;
-Matching* matchPtr;
+TransformEstimator te;
+string name;
+float weight;
 
-void conf_callback(calib::NodeConfig &config, uint32_t level)
+void callback(const calib::MatchesConstPtr& matches)
 {
-    freq = config.freq;
-    switch(config.logger)
+    te.reset();
+    vector<Eigen::Vector4f> planes1;
+    vector<Eigen::Vector4f> planes2;
+    vector<Eigen::Vector3f> points1;
+    vector<Eigen::Vector3f> points2;
+    for (int i = 0; i < matches->planes1.size(); i++)
     {
-      case 0:
-        if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) 
-        {
-          ros::console::notifyLoggerLevelsChanged();
-        }
-        break;
-      case 2:
-        if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Warn)) 
-        {
-          ros::console::notifyLoggerLevelsChanged();
-        }
-        break;
-      case 3:
-        if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Fatal)) 
-        {
-          ros::console::notifyLoggerLevelsChanged();
-        }
-        break;
-      default:
-        if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info)) 
-        {
-          ros::console::notifyLoggerLevelsChanged();
-        }
+      planes1.push_back(Eigen::Vector4f(matches->planes1[i].coef[0], matches->planes1[i].coef[1], matches->planes1[i].coef[2], matches->planes1[i].coef[3]));
+      planes2.push_back(Eigen::Vector4f(matches->planes2[i].coef[0], matches->planes2[i].coef[1], matches->planes2[i].coef[2], matches->planes2[i].coef[3]));
     }
+    pcPtr keypoints1(new pc_t);
+    convert(matches->points1, keypoints1);
+    pcPtr keypoints2(new pc_t);
+    convert(matches->points2, keypoints2);
+    for (int i = 0; i < keypoints1->points.size(); i++)
+    {
+      points1.push_back(Eigen::Vector3f(keypoints1->points[i].x, keypoints1->points[i].y, keypoints1->points[i].z));
+      points2.push_back(Eigen::Vector3f(keypoints2->points[i].x, keypoints2->points[i].y, keypoints2->points[i].z));
+    }
+    // points1.push_back(Eigen::Vector3f(0,0,0));
+    // points2.push_back(Eigen::Vector3f(0,0,0));
+    te.addPlanes(planes1, true, weight);
+    te.addPlanes(planes2, false, weight);
+    te.addPoints(points1, true);
+    te.addPoints(points2, false);
+    Eigen::Affine3d T(te.getTransform());
+
+    tf::Transform transform;
+    tf::transformEigenToTF(T, transform);
+    static tf::TransformBroadcaster br;
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "cam_center", name));
 }
 
-void planes_callback(const calib::PlanesConstPtr& planes1, const calib::PlanesConstPtr& planes2)
+void conf_callback(calib::TransformEstimationConfig &config, uint32_t level)
 {
-    matchPtr->update(planes1, planes2);
+  weight = config.planes_weight;
 }
 
 int main(int argc, char *argv[])
 {
 ///////////
 // init
- 
     string name1 = argv[1];
     string name2 = argv[2];
-    string topic = "/calib/planes/";
-    string topic1 = topic + name1 + "/planes";
-    string topic2 = topic + name2 + "/planes"; 
-    string node_name = (string) "transform_estimation_" + name1 + "_" + name2;
+    name = name1 + "_" + name2;
+    string topic = "/calib/" + name + "/matches";
+    string node_name = (string) "transform_estimation_" + name;
     ros::init(argc, argv, node_name);
-    ros::NodeHandle nh("/calib/" + name1 + "_" + name2);
-    ros::NodeHandle node_te("/calib/" + name1 + "_" + name2 + "/transform_estimation");
+    ros::NodeHandle nh;
+    ros::NodeHandle node_te("/calib/" + name + "/transform_estimation");
 
-    message_filters::Subscriber<calib::Planes> cam1_sub(nh, topic1, 1);
-    message_filters::Subscriber<calib::Planes> cam2_sub(nh, topic2, 1);
-    typedef sync_policies::ApproximateTime<calib::Planes, calib::Planes> KinectSync;
-    Synchronizer<KinectSync> sync(KinectSync(10), cam1_sub, cam2_sub);
-    sync.registerCallback(boost::bind(&planes_callback, _1, _2));
+    ros::Subscriber sub = nh.subscribe(topic, 1, &callback);
 
-    dynamic_reconfigure::Server<calib::NodeConfig> server(nh);
-    dynamic_reconfigure::Server<calib::NodeConfig>::CallbackType f;
+    dynamic_reconfigure::Server<calib::TransformEstimationConfig> server(node_te);
+    dynamic_reconfigure::Server<calib::TransformEstimationConfig>::CallbackType f;
     f = boost::bind(&conf_callback, _1, _2);
     server.setCallback(f);
 
   ///////////
 // main code
   
-  Matching match(&node_te, name1, name2);
-  matchPtr = &match; 
-  dynamic_reconfigure::Server<calib::MatchingConfig> server_matching(node_te);
-  dynamic_reconfigure::Server<calib::MatchingConfig>::CallbackType f_match;
-  f_match = boost::bind(&Matching::conf_callback, &match, _1, _2);
-  server_matching.setCallback(f_match);
  
  ////////////
 // ros loop
